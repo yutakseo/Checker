cbd6e70e74f9c42ea1d170fb5de6416b4356da7d
yutak <soano2428@gmail.com>
2023-09-12 10:41:44 +0900

    23.09.12 메인코드 작성

--- modelEvaluation.py
+++ modelEvaluation.py
@@ -1,33 +1,41 @@
+import math
 from TEST_DATASET import *
 from createArray import visual_array
 
-
-
+def calculate_distance(p1, p2):
+    return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
 
 def evaluation(answer, output):
     #정답데이터의 코너 개수와 좌표
-    num_of_corner = 0
     corner_position = []
     #출력데이터의 코너 개수와 좌표
-    recognized_output_corner = 0
     output_position = []
     
     #이중배열 반복문 실행
     for i in range(len(answer)):
         for j in range(len(answer[0])):
             if answer[i][j] != 0:
-                num_of_corner += 1
                 corner_position.append([i,j])
-                
             if output[i][j] != 0:
-                recognized_output_corner += 1
                 output_position.append([i,j])
     
-    if num_of_corner != recognized_output_corner:
-        if num_of_corner > recognized_output_corner:
-            return recognized_output_corner/num_of_corner*100
-        elif recognized_output_corner > num_of_corner:
-            return (num_of_corner-(recognized_output_corner-num_of_corner))/num_of_corner
+    #정답데이터의 코너 개수와 출력데이터의 코너 개수가 다를 경우의 평가
+    if len(corner_position) != len(output_position):
+        if len(corner_position) > len(output_position):
+            return len(output_position)/len(corner_position)*100
+        elif len(output_position) > len(corner_position):
+            return (len(corner_position)-(len(output_position)-len(corner_position)))/len(corner_position)
+    #두 데이터의 개수가 같을 때
     else:
-        for i in corner_position:
-            ㅇㅇㅇ
+        closest_pair = None
+        min_distance = float('inf')  # 초기 최소 거리를 무한대로 설정
+
+        for point1 in corner_position:
+            for point2 in output_position:
+                distance = calculate_distance(point1, point2)
+                if distance < min_distance:
+                    min_distance = distance
+                    closest_pair = {point1, point2}
+        print(closest_pair)
+
+evaluation(test_data_mini, test_data_mini_compare)
